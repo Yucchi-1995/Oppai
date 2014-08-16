7ba57e2635ebfc0d0184d13fc66519a2fc7082ac
Yucchi <tiger@yucchi-ja.com>
2014-08-16 17:41:55 +0900

    初めてのコミット

---
+++ src/jp/yucchi/firsttimegithub/FirstTimeGitHub.java
@@ -0,0 +1,16 @@
+package jp.yucchi.firsttimegithub;
+
+/**
+ *
+ * @author Yucchi
+ */
+public class FirstTimeGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
