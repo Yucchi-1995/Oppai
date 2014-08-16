9dbfdede85bdabef76ed732aa64f4f4afea938a4
Yucchi <tiger@yucchi-ja.com>
2014-08-16 18:39:29 +0900

    暗号文を追加

--- src/jp/yucchi/firsttimegithub/FirstTimeGitHub.java
+++ src/jp/yucchi/firsttimegithub/FirstTimeGitHub.java
@@ -10,7 +10,8 @@ public class FirstTimeGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("この暗号文を解読せよ。");
+        System.out.println("15,16,16,1,9,23,15,23,1,18,1,21,13,15,14,15,8,1,15,16,16,1,9,14,1,11,21");
     }
 
 }
